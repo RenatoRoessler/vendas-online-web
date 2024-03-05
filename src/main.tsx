5114d1a4d0ae5ed048efb291ae6422f566258b2b
renato <renato.roessler@pro22.com>
2024-03-05 19:19:56 -0300

    feat: insert react-router

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,22 @@ import './index.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouteObject, RouterProvider } from 'react-router-dom';
 
-import App from './App.tsx';
+import { loginRoutes } from './modules/login/routes';
+
+export const mainRouter: RouteObject[] = [
+  {
+    path: '/',
+    element: <div>Pagina princial</div>,
+    errorElement: <div>Not Found</div>,
+  },
+];
+
+const router = createBrowserRouter([...loginRoutes, ...mainRouter]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
