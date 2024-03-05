6d39b9e42a4deb0df6bf2272460092034a40dab4
renato <renato.roessler@pro22.com>
2024-03-05 19:04:06 -0300

    feat: insert file svg

--- src/modules/login/screens/LoginScreen.tsx
+++ src/modules/login/screens/LoginScreen.tsx
@@ -1,30 +1,47 @@
+import axios from 'axios';
 import { useState } from 'react';
 
 import Button from '../../../shared/button/Button';
+import SVGLogo from '../../../shared/button/icons/SVGLogo';
 import Input from '../../../shared/Inputs/Input';
 import {
   BackgroundImage,
   CointainerLoginScreen,
   ContainerLogin,
   LimitedContainer,
-  LogoImage,
   TitleLogin,
 } from '../styles/loginScreen.styles';
 
 const LoginScreen = () => {
-  const [userName, setUserName] = useState('');
+  const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  const handleUserName = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setUserName(event.target.value);
+  const handleEmail = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
   };
 
   const handlePassword = (event: React.ChangeEvent<HTMLInputElement>) => {
     setPassword(event.target.value);
   };
 
-  const handleLogin = () => {
-    console.log({ userName, password });
+  const handleLogin = async () => {
+    const returnObject = await axios({
+      method: 'post',
+      url: 'http://localhost:8080/auth',
+      data: {
+        email,
+        password,
+      },
+    })
+      .catch(() => {
+        alert('Usuário ou senha invalida');
+      })
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      .then((result: any) => {
+        alert(`'Login efetuado com sucesso' ${result.data.accessToken}`);
+        return result;
+      });
+    console.log(returnObject);
   };
 
   return (
@@ -32,11 +49,11 @@ const LoginScreen = () => {
       <BackgroundImage src="./background.png" />
       <ContainerLogin>
         <LimitedContainer>
-          <LogoImage src="logo.png" />
+          <SVGLogo />
           <TitleLogin level={2} type="secondary">
             LOGIN
           </TitleLogin>
-          <Input title="USUÁRIO" margin="32px 0px 0px" value={userName} onChange={handleUserName} />
+          <Input title="USUÁRIO" margin="32px 0px 0px" value={email} onChange={handleEmail} />
           <Input
             title="SENHA"
             margin="32px 0px 0px"
